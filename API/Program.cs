52be15ef5d7645452d5e515d7ce509b3e1ce48a8
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-01-29 07:14:49 +0530

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StoreContext>(options=>options.UseSqlite(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
