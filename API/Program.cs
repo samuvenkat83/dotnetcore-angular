bb72221f54c20a763b6d1314ed38e06d0c26ad14
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-02-01 12:05:44 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-
+using Core.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddDbContext<StoreContext>(options=>options.UseSqlite(
     builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -26,5 +28,23 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var host = WebApplication.CreateBuilder(args).Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+} 
+catch(Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+
+}
+host.Run();
 
 app.Run();
