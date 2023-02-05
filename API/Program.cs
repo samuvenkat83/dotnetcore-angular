66c79b05d03f186a7eb19112f84dd4b6348108c7
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-02-05 11:09:37 +0530

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,8 @@ using Core.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddDbContext<StoreContext>(options=>options.UseSqlite(
     builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -14,37 +16,46 @@ builder.Services.AddDbContext<StoreContext>(options=>options.UseSqlite(
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger();    
     app.UseSwaggerUI();
 }
 
+app.UseCors("corsapp");
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-var host = WebApplication.CreateBuilder(args).Build();
+//var host = WebApplication.CreateBuilder(args).Build();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-var context = services.GetRequiredService<StoreContext>();
-var logger = services.GetRequiredService<ILogger<Program>>();
-try
-{
-    await context.Database.MigrateAsync();
-    await StoreContextSeed.SeedAsync(context);
-} 
-catch(Exception ex)
-{
-    logger.LogError(ex, "An error occured during migration");
+//using var scope = app.Services.CreateScope();
+//var services = scope.ServiceProvider;
+//var context = services.GetRequiredService<StoreContext>();
+//var logger = services.GetRequiredService<ILogger<Program>>();
+//try
+//{
+//    await context.Database.MigrateAsync();
+//    await StoreContextSeed.SeedAsync(context);
+//} 
+//catch(Exception ex)
+//{
+//    logger.LogError(ex, "An error occured during migration");
 
-}
-host.Run();
+//}
+//host.Run();
 
 app.Run();
