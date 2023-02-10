2b12add2f5af74b3f9e04dbe77311b75624d8f7a
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-02-11 03:22:54 +0530

    End of Section 12

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
   
-  constructor() {}
+  constructor() {} 
 
   ngOnInit(): void {    
   }
