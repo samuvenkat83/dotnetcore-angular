4ebec9304261d5a8bd664754d7303fcb0757cc64
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-02-15 23:08:38 +0530

    End of Section 15

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,16 @@
 using API.Middileware;
 using API.Extensions;
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Core.Entities.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,8 +27,27 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
+} 
+catch(Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
---
+++ Core/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+
+namespace Core.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+        
+    }
+}
