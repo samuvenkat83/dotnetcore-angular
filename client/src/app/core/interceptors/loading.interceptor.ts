fa0046f1692f318272d9c1c2a2dc244c60aba614
Samudrala Venkatesh <Samudrala.Venkatesh@gds.ey.com>
2023-02-20 22:14:48 +0530

    End of section 7

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -8,13 +8,15 @@ import {
 import { delay, finalize, Observable } from 'rxjs';
 import { BusyService } from '../services/busy.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class LoadingInterceptor implements HttpInterceptor {
 
-  constructor(private busyService: BusyService) {}
+  constructor(private busyService: BusyService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.busyService.busy();
+    if (!request.url.includes('emailExists')) {
+      this.busyService.busy();
+    }
     return next.handle(request).pipe(
       delay(1000),
       finalize(() => this.busyService.idle())
